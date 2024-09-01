e89fc07acc6597168b7e5228bb0701c5c3fc8353
Vanesh37 <110464597+Vanesh37@users.noreply.github.com>
2024-09-01 18:59:55 +0530

    Update db_connection.php

--- main/db_connection.php
+++ main/db_connection.php
@@ -4,7 +4,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "tester";
+$dbname = "main";
 
 // create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
